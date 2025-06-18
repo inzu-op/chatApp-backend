a03e2731dc75e3c3149600f742dd6ec88656203b
inzu-op <samoraikonzan888@gmail.com>
2025-06-18 21:47:17 +0530

    Over

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chatapp-front-jet.vercel.app/", 
+    origin: "https://chatapp-front-jet.vercel.app", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
@@ -25,7 +25,7 @@ declare global {
 global.io = io;
 
 app.use(cors({
-  origin: "https://chatapp-front-jet.vercel.app/", // allow frontend
+  origin: "https://chatapp-front-jet.vercel.app", // allow frontend
   credentials: true, // if you're using cookies or sessions
 }))
 app.use(express.json());
